6c700227a443034ed7c29d913d671652387e8e8f
Rubens Bispo <rubensbispo29@gmail.com>
2024-04-08 21:16:49 -0300

    Index

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+
+	
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Login</title>
+</head>
+<body>
+	<h1>Bem vindo - entre com seu dados</h1>
+</body>
+</html>
