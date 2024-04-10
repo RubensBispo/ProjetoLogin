b780a4f0ae892759ed27d41e17d0aaf8a836612f
Rubens Bispo <rubensbispo29@gmail.com>
2024-04-10 19:21:48 -0300

    Add files via upload

---
+++ dbDados.sql
@@ -0,0 +1,16 @@
+create database dbDados;
+
+use dbDados;
+
+
+create table pessoa(
+codigo int not null auto_increment,
+nome varchar(30) not null,
+sobrenome varchar(50) not null,
+Eemail varchar(100),
+cpf char(11),
+sexo char(1),
+usuário varchar(30),
+senha varchar(50),
+primary key(codigo)
+);
