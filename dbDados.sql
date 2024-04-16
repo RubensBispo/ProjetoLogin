0e68e815d961d9fde382014dadf4a49c4a3206dc
Rubens Bispo <rubensbispo29@gmail.com>
2024-04-15 21:12:53 -0300

    Corrigindo script banco

--- dbDados.sql
+++ dbDados.sql
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
+
 create database dbDados;
 
 use dbDados;
-
 
 create table pessoa(
 codigo int not null auto_increment,
@@ -14,21 +13,4 @@ sexo char(1),
 usuário varchar(30),
 senha varchar(50),
 primary key(codigo)
-=======
-create database dbDados;
-
-use dbDados;
-
-
-create table pessoa(
-codigo int not null auto_increment,
-nome varchar(30) not null,
-sobrenome varchar(50) not null,
-Eemail varchar(100),
-cpf char(11),
-sexo char(1),
-usuário varchar(30),
-senha varchar(50),
-primary key(codigo)
->>>>>>> 11442b24710ed018204b164e3948c28a85f718e7
 );
